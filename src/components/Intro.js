f6ab94ed9c279d645af7cb5a9f2bee1256b312ad
Evi Dianasari <evidnsr@gmail.com>
2023-08-12 23:56:21 +0800

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -80,8 +80,8 @@ const Intro = () => {
             <SubBox>
                 <Text>
                     <h1>Hi,</h1>
-                    <h3>I'm CodeBucks.</h3>
-                    <h6>I design and Code simple yet beautiful websites.</h6>
+                    <h3>This Website from.</h3>
+                    <h6>KKN-PPM XXVII UNIVERSITAS UDAYANA 2023</h6>
                 </Text>
             </SubBox>
             <SubBox>
